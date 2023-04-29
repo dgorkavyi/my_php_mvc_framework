7e033334fe52dc7d796ab33098561e44799b902b
dgorkavyi <dima.net.dd@gmail.com>
2023-04-29 12:31:58 +0300

    init

---
+++ src/index.php
@@ -0,0 +1,21 @@
+<?php
+// $servername = 'db';
+// $username = $_ENV['MYSQL_USER'];
+// $password = $_ENV['MYSQL_PASSWORD'];
+
+// // Create connection
+// $conn = new mysqli($servername, $username, $password);
+
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+// echo "Connected successfully";
+
+// print("<pre>".print_r($_SERVER,true)."</pre>");
+
+
+require_once 'application/lib/dev.php';
+?>
+
+<h1>HELLO</h1>
